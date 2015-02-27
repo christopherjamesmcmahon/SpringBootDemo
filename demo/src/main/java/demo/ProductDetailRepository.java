82343e472bd51c78e72ac5d2ef040502878b584c
Chris <christopher.james.mcmahon@gmail.com>
2015-02-27 15:56:37 +0100

    First version

---
+++ demo/src/main/java/demo/ProductDetailRepository.java
@@ -0,0 +1,6 @@
+package demo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ProductDetailRepository extends CrudRepository <ProductDetail, String>{
+}
